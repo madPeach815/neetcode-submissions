04627d6ca286309dc7dd1ef7526435642d65039b
Jemma Lee <271130229+madPeach815@users.noreply.github.com>
2026-09-02 10:37:59 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,34 @@
+//make another stack for min value. and everytime when we push number in original stack, the minValue stack remember the minimum value at the moment. Just using min_val int is not enought because it will only hold one last result so every time when we pop() the original stack it will have a problem. 
+
+class MinStack {
+
+    private Stack<Integer> mainStack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        this.mainStack = new Stack<>();
+        this.minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+        if(minStack.isEmpty()){
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+    }
+    
+    public void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return mainStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
